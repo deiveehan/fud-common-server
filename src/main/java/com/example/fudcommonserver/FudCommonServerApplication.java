703be193adf45bdeef417e9d3885d2a73af76f13
Deiveehan Nallazhagappan <deivee@cmdspace.local>
2022-05-14 03:15:23 -0600

    init

---
+++ src/main/java/com/example/fudcommonserver/FudCommonServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.fudcommonserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FudCommonServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FudCommonServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fudcommonserver/FudCommonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fudcommonserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FudCommonServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
